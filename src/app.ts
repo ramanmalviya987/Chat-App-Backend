14b9196f27382e3b734cb256511185dd30a11153
Raman Malviya <raman.s.malviya@gmail.com>
2026-07-09 01:41:16 +0530

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import healthRoutes from "./routes/health.routes.js";
-import routes from './routes/index.js'
+import routes from "./routes/index.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
-
-
+import cors from "cors";
 
 const app = express();
 
-
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173",
+        //   "http://192.168.1.101:5173",
+        ],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
-app.use('/api/v1', healthRoutes);
+app.use("/api/v1", healthRoutes);
 app.use("/api/v1", routes);
 
 app.use(errorMiddleware);
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import app from "./app.js";
 import "dotenv/config";
 
 
-const PORT = 3000;
+const PORT = 8000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+// });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
