e1cff3bef31eef70f5ccbb345d03b0d3c2899f81
Raman Malviya <raman.s.malviya@gmail.com>
2026-07-14 12:22:30 +0530

    undo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,11 +4,7 @@ import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString,
-    ssl: {
-        rejectUnauthorized: true,
-    }
- });
+const adapter = new PrismaPg({ connectionString });
 
 const prisma = new PrismaClient({ adapter });
 
